28eff312867ad6d8a89e7fb9a9e3dc349a8d6094
vitalii.babii-ext@man.eu <5QvTtR&fH?>
2023-05-12 11:19:08 +0200

    rename

--- utils/test.js
+++ utils/test.js
@@ -1,12 +1,12 @@
-const Helper = require('./helper');
-console.log(Helper.formatDate(Helper.getDate(),'M.Y'));
+//const Helper = require('./helper');
+//console.log(Helper.formatDate(Helper.getDate(),'M.Y'));
 
 
-const { readdirSync, rename,renameSync } = require('fs');
+const { readdirSync, rename, renameSync, lstatSync } = require('fs');
 const { resolve } = require('path');
 
 // Get path to image directory
-const imageDirPath = resolve(__dirname, '../models');
+const imageDirPath = resolve(__dirname, '../tasks');
 
 // Get an array of the files inside the folder
 const files = readdirSync(imageDirPath);
@@ -14,16 +14,20 @@ console.log(files)
 // Loop through each file that was retrieved
 files.forEach(file => {
     const oldPath = imageDirPath + `/${file}`;
+    if(lstatSync(oldPath).isFile() && file !== 'test.js'){
+        // lowercasing the filename
+        let newPath = imageDirPath + `/${file.toLowerCase()}`;
+        //newPath = newPath.substr(0,newPath.length-4)
+        console.log(newPath);
+        // Rename file
+        /*rename(
+            oldPath,
+            newPath,
+            err => err?console.log(err):null
+        );*/
+        renameSync(oldPath, newPath+'.bkp');
+        //renameSync(newPath/*+'.bkp'*/, newPath);
+        //renameSync(oldPath/*+'.bkp'*/, newPath);
 
-    // lowercasing the filename
-    const newPath = imageDirPath + `/${file.toLowerCase()}`;
-    console.log(newPath);
-    // Rename file
-    /*rename(
-        oldPath,
-        newPath,
-        err => err?console.log(err):null
-    );*/
-    //renameSync(oldPath, newPath+'.bkp');
-    //renameSync(newPath+'.bkp', newPath);
+    }
 });
