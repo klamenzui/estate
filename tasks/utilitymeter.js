407e935d9c0613f83717390f3f58f68818fb86a3
vitalii.babii-ext@man.eu <5QvTtR&fH?>
2023-05-12 11:22:12 +0200

    rename

---
+++ tasks/utilitymeter.js
@@ -0,0 +1,69 @@
+const Helper = require('../utils/helper');
+const Model = require('../models/utilitymeter');
+const t_scheme = require('../models/db/tables/utilitymeter');
+const t_utilityservice = require('../models/db/tables/utilityservice');
+const SiteParser = require('../models/siteparser')
+
+class Utilitymeter {
+    constructor() {
+    }
+
+    async exec() {
+        console.log('---Utilitymeter---');
+        let model = new Model();
+        let res = await model.get();
+        if (res && res.rows) {
+            for (let i in res.rows) {
+                let meter = res.rows[i];
+                console.log('Utilitymeter:',meter);
+                if(!Helper.isEmpty(meter[t_utilityservice.tariff_url.name])&&!Helper.isEmpty(meter[t_utilityservice.tariff_selector.name])){
+                    let id = meter[t_scheme._primary_key];
+                    let url = meter[t_utilityservice.tariff_url.name];
+                    let selector = meter[t_utilityservice.tariff_selector.name].split("\n");
+                    console.log('tariff_url:',url);
+                    console.log('tariff_selector:',selector);
+                    let sp = new SiteParser();
+                    sp.parse(url,selector,function (res) {
+                        res=Helper.toFloat(res);
+                        if(res){
+                            let row = {};
+                            row[t_scheme._primary_key]=id;
+                            row[t_scheme.price.name]=res;
+                            console.log(row);
+                            console.log(meter);
+                            try {
+                                model.update(row).then(result => console.log("Result:", result))
+                                    .catch(e => console.log("err:", e));
+                            }catch (e) {
+                                console.log(e);
+                            }
+                        }
+
+
+                    });
+                    let eid = meter[t_scheme.estate_id.name];
+                    let chat = await app.locals.bot.bot_chat.getByTitle(eid);
+                    console.log('chat:', chat);
+                    let monthDiff = Helper.getMonthDifference(new Date(meter[t_scheme.date.name]), new Date());
+                    console.log('monthDiff:', monthDiff);
+                    if (chat && chat.rows) {
+                        chat = chat.rows[0];
+                        if(chat){
+                            if(monthDiff > 0){
+                                await app.locals.bot.sendMessage(chat, "Пожалуйста, заполните показания счетчика");// указав также его номер("+id+"). Пример:\nпоказания счетчика "+id+"=123456");
+                            }
+                        } else {
+                            console.log('chat was not found:',eid);
+                        }
+                    }
+                }
+
+            }
+        }
+        /*
+        new Contract().debt(-1,(results) => {
+            console.log(results);
+        });*/
+    }
+}
+module.exports = new Utilitymeter();
--- utils/test.js
+++ utils/test.js
@@ -6,7 +6,7 @@ const { readdirSync, rename, renameSync, lstatSync } = require('fs');
 const { resolve } = require('path');
 
 // Get path to image directory
-const imageDirPath = resolve(__dirname, '../tasks');
+const imageDirPath = resolve(__dirname, '../utils');
 
 // Get an array of the files inside the folder
 const files = readdirSync(imageDirPath);
@@ -17,7 +17,7 @@ files.forEach(file => {
     if(lstatSync(oldPath).isFile() && file !== 'test.js'){
         // lowercasing the filename
         let newPath = imageDirPath + `/${file.toLowerCase()}`;
-        //newPath = newPath.substr(0,newPath.length-4)
+        newPath = newPath.substr(0,newPath.length-4)
         console.log(newPath);
         // Rename file
         /*rename(
@@ -25,9 +25,9 @@ files.forEach(file => {
             newPath,
             err => err?console.log(err):null
         );*/
-        renameSync(oldPath, newPath+'.bkp');
+        //renameSync(oldPath, newPath+'.bkp');
         //renameSync(newPath/*+'.bkp'*/, newPath);
-        //renameSync(oldPath/*+'.bkp'*/, newPath);
+        renameSync(oldPath/*+'.bkp'*/, newPath);
 
     }
 });
